4b751395d136d16aadb2787fc633bf86b026437b
Yannick Schutz <yannick.schutz@gmail.com>
2017-02-19 15:36:53 +0100

    Add route for root in regions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -8,4 +8,14 @@ class RegionsController < ApplicationController
                         terms: ::Configuration.search_terms)
     end
   end
+
+  def root
+    @houses = Rails.cache.fetch("regions-#{region}-houses", expires_in: 10.minutes) do
+      SeLogerApi.search(postal_code: "51,21,58,71,89",
+                        max_price: ::Configuration.max_price,
+                        min_price: ::Configuration.min_price,
+                        terms: ::Configuration.search_terms)
+    end
+    render :show
+  end
 end

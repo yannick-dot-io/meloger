057899e7d4ba8ca990f5827d0ae553f22656fbc9
Yannick Schutz <yannick.schutz@gmail.com>
2017-02-19 15:40:50 +0100

    Add default regions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -9,13 +9,17 @@ class RegionsController < ApplicationController
     end
   end
 
-  def root
-    @houses = Rails.cache.fetch("regions-#{region}-houses", expires_in: 10.minutes) do
-      SeLogerApi.search(postal_code: "51,21,58,71,89",
+  def index
+    @houses = Rails.cache.fetch("regions-#{default_regions}-houses", expires_in: 10.minutes) do
+      SeLogerApi.search(postal_code: default_regions,
                         max_price: ::Configuration.max_price,
                         min_price: ::Configuration.min_price,
                         terms: ::Configuration.search_terms)
     end
     render :show
+  end
+
+  def default_regions
+    ENV.fetch("REGIONS", "51,21,58,71,89")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "regions#root"
-  resources :regions, only: %w{show}
+  root to: "regions#index"
+  resources :regions, only: %w{index show}
 end

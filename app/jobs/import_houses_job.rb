63fe4ceb88109ee076b8cf6a5e565458fa5e9b93
Yannick Schutz <me@yannick.io>
2017-03-05 14:34:48 +0100

    Keep id alive

--- app/jobs/import_houses_job.rb
+++ app/jobs/import_houses_job.rb
@@ -1,8 +1,19 @@
 # Replace records with new ones for houses
 class ImportHousesJob < ApplicationJob
   def perform(_args = {})
-    House.delete_all
-    houses.map(&:save)
+    House.where.not(external_id: houses.map(&:external_id)).delete_all
+    houses.each do |h|
+      house = House.find_or_initialize_by(external_id: h.external_id)
+      house.postal_code = h.postal_code
+      house.price       = h.price
+      house.permalink   = h.permalink
+      house.title       = h.title
+      house.description = h.description
+      house.created_at  = h.created_at
+      house.updated_at  = h.updated_at
+      house.payload     = h.payload
+      house.save
+    end
   end
 
   def houses

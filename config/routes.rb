d5921be04ad4dfe4ef8dadeea60387745a4b22d2
Yannick Schutz <me@yannick.io>
2017-03-05 14:40:28 +0100

    Remove regions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "houses#index"
-  resources :regions, only: %w{show}
   resources :houses, only: %w{index show}
 end

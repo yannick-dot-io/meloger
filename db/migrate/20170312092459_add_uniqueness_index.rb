5fb1a256224399467c0fcda7139e816ea7d5e41a
Yannick Schutz <me@yannick.io>
2017-03-12 10:27:24 +0100

    Unique external_id

--- db/migrate/20170312092459_add_uniqueness_index.rb
+++ db/migrate/20170312092459_add_uniqueness_index.rb
@@ -1,7 +1,5 @@
 class AddUniquenessIndex < ActiveRecord::Migration[5.0]
   def change
-    def change
-      add_index :houses, :external_id, unique: true
-    end
+    add_index :houses, :external_id, unique: true
   end
 end
